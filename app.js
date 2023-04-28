fc7dd6282e9a99e016c3f75e5944f8d93350aa2b
kipgeoffry <kipgeoffry@gmail.com>
2023-04-28 23:17:09 +0300

    created home route and updated as middleware on entrypoint app

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+// const {hrouter} = require("./routes/home"); //same as import router from "./routes/home"
+const homeUrl = require("./routes/home");
 const port = 4500;
 
 const app = express();
 
-// app.get('PORT',4500);
+// app.set('PORT',4500);
+app.use(homeUrl.hrouter); //middleware that routes request with url "/" to the logic that handles it
 
 app.listen(port,()=>{
-    console.log(`application is running and listening on port ${port}`)
+    console.log(`application is running and listening on port ${port}`);
 });
