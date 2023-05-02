e8243cd2fbb6fa327e18fa2c5fe0482c2cfa9151
kipgeoffry <kipgeoffry@gmail.com>
2023-05-02 20:26:50 +0300

    created app dashboard

--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,12 @@ hrouter.get("/",(req,res)=>{
     // res.send("Access to this page is successful");
     res.render('index',{});
 });
+hrouter.post("/update",(req,res)=>{
+    // res.send("updated successfully")
+    console.log("request received")
+    // console.log(req)
+
+});
 
 exports.hrouter = hrouter; //another way of exporting modules
 // module.exports = router
