6df2cfdc399c84cf6f4de134b3ddd2cd272fbb9e
kipgeoffry <kipgeoffry@gmail.com>
2023-05-11 22:01:48 +0300

    created stores url route and added logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const {hrouter} = require("./routes/home"); //same as import router from "./routes/home"
 const homeUrl = require("./routes/home");
+const storeRoute = require("./routes/stores");
 // const bodyParser = require('body-parser')
 const port = 4500;
 
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(checkUrl);//middleware that will console log the method and url that is accesses
 app.use(homeUrl); //middleware that routes request with url "/" to the logic that handles it
+app.use("/api/v1/stores",storeRoute); //url route to stores 
 
 
 app.set('view engine', 'ejs');
--- routes/home.js
+++ routes/home.js
@@ -27,7 +27,7 @@ const links = {
     "capacity": "IFB 5Mbps"
 };
 
-const family = {
+const family = [{
     name: "kigen",
     Age: 20,
     family: {
@@ -35,42 +35,16 @@ const family = {
         mother: "penina",
         children: ["june", "Jael"]
     }
-};
-const shoppingList = ["banana","lemon","cabbage","carrot"]
-
-const stores = [
-    {
-      id: 1,
-      name: 'Walmart',
-      distance: 2.5, // in miles
-      rating: 2.2,
-    },
-    {
-      id: 2,
-      name: 'Target',
-      distance: 3.8, // in miles
-      rating: 3.5,
-    },
-    {
-      id: 3,
-      name: 'Costco',
-      distance: 6.2, // in miles
-      rating: 4.8,
-    },
-    {
-      id: 4,
-      name: 'Safeway',
-      distance: 1.1, // in miles
-      rating: 3.9,
-    },
-    {
-      id: 5,
-      name: 'Whole Foods',
-      distance: 4.5, // in miles
-      rating: 4.7,
+},
+{
+    name:"evans",
+    Age:30,
+    family:{
+        father:"John",
+        siblings:["mark","Grace","Jeptoo"]
     }
-    // add more stores as needed
-  ];
+}
+];
 
 hrouter.get("/family", (req, res) => {
     console.log(req.params);
@@ -86,29 +60,6 @@ hrouter.get("/family", (req, res) => {
 
 });
 
-//this code filters stores based on ratings it is using query parameter//use filter method
-hrouter.get("/stores",(req,res)=>{
-    const {rating} = req.query;
-    const parsedRating = parseInt(rating);
-    if(!isNaN(parsedRating)){
-        const findStore = stores.filter(store=>store.rating <= parsedRating);
-        res.json(findStore);
-    } else res.json(stores);   ////this code dispalys all available stores
-    
-})
-
- //this code finds a store when store id is provided using route parameters //use find methos
-hrouter.get("/stores/:id",(req,res)=>{
-    const {id} = req.params;
-    const findStore = stores.find(store=>store.id == id);
-    res.json(findStore);
-});
-
-//this code adds a store in the stores array
-hrouter.post("/stores",(req,res)=>{
-    stores.push(req.body);
-    res.send("store added successfully")
-})
 
 
 hrouter.get("/links/:value",(req,res)=>{
